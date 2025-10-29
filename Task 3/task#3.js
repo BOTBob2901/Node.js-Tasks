5a60a8a27a11aa364689949a55b377a959d438ae
Igor Nikonov <128256749+BOTBob2901@users.noreply.github.com>
2025-10-29 11:19:41 +0200

    Update task#3.js

--- Task 3/task#3.js
+++ Task 3/task#3.js
@@ -1,9 +1,15 @@
-"use strict"
+"use strict";
 // Igor Nikonov, Saher Haddad
-const nums = [1,0,3,0,0,5,6,7,8,0];
-let count = 0;
+
+const nums = [1, 0, 3, 0, 0, 5, 6, 7, 8, 0]; // Array of numbers
+let count = 0; // Counter for zeros
+
+// Loop through each number and count how many are zeros
 nums.forEach((num) => {
-  num === 0 ? count ++ : count += 0; 
-}); 
+  num === 0 ? count++ : count += 0;
+});
+
+// Print total number of zeros found
 console.log(count);
+
 
