a6378df653e0d81ec6e2e97c372c3f51ac28d7cd
Igor Nikonov <128256749+BOTBob2901@users.noreply.github.com>
2025-10-29 11:17:32 +0200

    Update task#2.js

--- Task 2/task#2.js
+++ Task 2/task#2.js
@@ -1,14 +1,19 @@
 "use strict"
 // Igor Nikonov, Saher Haddad
+
+// Loop from 2 to 236 and print prime numbers
 for (let j = 2; j < 237; j++) {
   if(is_prime(j))
     console.log(j)
 }
 
-function is_prime(num){
+// Function to check if a number is prime
+function is_prime(num) {
+  // Check divisibility from 2 up to the square root of the number
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if ( num% i === 0) 
-      return false;
+    if (num % i === 0) 
+      return false; // Not prime if divisible
   }
-  return true
+  return true // Prime if no divisors found
 }
+
