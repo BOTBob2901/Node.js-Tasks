87d9dbfd42b642edd92e1e0b0f68731cde27392b
Igor Nikonov <128256749+BOTBob2901@users.noreply.github.com>
2025-12-15 19:04:57 +0200

    Update logger.js

--- MiddlewareHw#2/logger.js
+++ MiddlewareHw#2/logger.js
@@ -1,6 +1,7 @@
-//logger.js
+//logger middleware: logs method, url and current date + time for every request
 const logger = (req, res, next) => {
   console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
   next();
 };
 module.exports = logger;
+
